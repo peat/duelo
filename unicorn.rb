0a17cfb715d81ae36df1420e2134e717194de2f2
Peat Bakke <peat@peat.org>
2010-10-01 15:45:59 -0700

    Cleaning up some cruft

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,6 @@
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 4
+worker_processes 2
 
 # working_directory "."
 
