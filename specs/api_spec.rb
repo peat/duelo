95d9d1f5958b0e3330fdb2ce8bc0bf5c386f55a1
Peat Bakke <peat@peat.org>
2010-10-02 10:49:08 -0700

    Updating challenge test to include health checks

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -5,13 +5,23 @@ API_VERSION = 'v1'
 describe "The API" do
 
   it "should respond correctly to a challenge request" do
+
+    health_before = request("/health?key=key")
     response = request("/challenge?from=1234&to=6789&skill=abcd")
+    health_after = request("/health?key=key")
     
     response['status'].should == "OK"
     response['challenge']['from'].should == "1234"
     response['challenge']['to'].should == "6789"
     response['challenge']['skill'].should == "abcd"
     response['challenge']['challenge'].length.should == 16
+
+    # check health to make sure a record was inserted
+    diff = 
+      health_after['challenge_count'] - 
+      health_before['challenge_count'] 
+
+    diff.should == 1
   end
 
   it "should have the proper parameters for a challenge request" do
