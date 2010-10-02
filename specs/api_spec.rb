99b935b92b34cd6a6d25130757d71079289d20b8
Peat Bakke <peat@peat.org>
2010-10-02 10:42:58 -0700

    Adding tests for health call

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -14,10 +14,26 @@ describe "The API" do
     response['challenge']['challenge'].length.should == 16
   end
 
+  it "should respond correctly to a health request" do
+    response = request("/health?key=key") 
+    
+    response['uptime'].should be_a( String )
+    response['uptime'].length.should > 0
+    response['character_count'].to_i.should be_an( Integer ) 
+    response['challenge_count'].to_i.should be_an( Integer ) 
+    response['history_count'].to_i.should be_an( Integer ) 
+  end
+
+  it "should require a key for a health request" do
+    response = request("/health")
+    response.should be_empty
+
+    response = request("/health?key=notkey")
+    response.should be_empty
+  end
+
   it "should have the proper parameters for a challenge request" do
     response = request("/challenge")
-
-    # empty request, needs parameters!
     response['status'].should_not == "OK"
   end
 
