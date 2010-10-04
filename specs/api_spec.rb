9c6981835e0e994af34f4074053f7c7e750c9f58
Peat Bakke <peat@peat.org>
2010-10-04 16:57:17 -0700

    Moving character tests into their own file

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -2,40 +2,6 @@ require './helper'
 
 describe "The API" do
 
-  it "should create a character" do
-    health_before = health_request
-    response = request( :post, "/character", { :name => "Peat's Mage" } )
-    health_after = health_request
-
-    response['status'].should == 'OK'
-    response['character']['id'].length.should == 16
-    response['character']['name'].should == "Peat's Mage"
-    response['character']['skills'].should be_empty
-  end
-
-
-  it "should update a character" do
-    c = new_character
-    s = new_skill
-
-    params = {
-      :character => c['id'],
-      :skills => [ s['id'] ]
-    }
-
-    response = request( :put, "/character", params )
-
-    response['status'].should == 'OK'
-    response['character']['skills'].should include( s['id'] )
-  end
-
-  it "should not allow you to change a character's name"
-
-  it "should not allow duplicate skills for a character"
-
-  it "should not allow skills that don't exist to be added to a character"
-
-
   it "should respond correctly to a challenge request" do
 
     charA = new_character
@@ -142,4 +108,5 @@ describe "The API" do
     response = request(:get, "/health", { :key => 'bad key' } )
     response['status'].should == 'ERROR'
   end
+
 end
---
+++ specs/character_spec.rb
@@ -0,0 +1,38 @@
+require './helper'
+
+describe "The Character API" do
+
+  it "should create a character" do
+    health_before = health_request
+    response = request( :post, "/character", { :name => "Peat's Mage" } )
+    health_after = health_request
+
+    response['status'].should == 'OK'
+    response['character']['id'].length.should == 16
+    response['character']['name'].should == "Peat's Mage"
+    response['character']['skills'].should be_empty
+  end
+
+
+  it "should update a character" do
+    c = new_character
+    s = new_skill
+
+    params = {
+      :character => c['id'],
+      :skills => [ s['id'] ]
+    }
+
+    response = request( :put, "/character", params )
+
+    response['status'].should == 'OK'
+    response['character']['skills'].should include( s['id'] )
+  end
+
+  it "should not allow you to change a character's name"
+
+  it "should not allow duplicate skills for a character"
+
+  it "should not allow skills that don't exist to be added to a character"
+
+end
